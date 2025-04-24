8c067a1bfcf79dc7e97584d709c8bb88310586ce
Braxton Hartley <brxtnhartley@gmail.com>
2025-04-24 13:55:31 -0500

    Update cash_register.py

--- cash_register.py
+++ cash_register.py
@@ -1,4 +1,5 @@
-
+inventory = {}
+prices = {}
 
 class CashRegister:
     
@@ -6,5 +7,25 @@ class CashRegister:
         self.__item = item
         self.__unit = unit
         self.__price = price
+        #self.__price = total
+        #self.__cart = cart
         
+    def purchase_item(self):
+        inventory[self.__item] = self.__unit
+        prices[self.__item] = self.__price
+        return inventory, prices
+    
+    def get_total(self):
+        total = 0
+        for item in prices:
+            print(f"this is item {item}")
+            total += prices[item]
+            print(f"this is total {total}")
+        pass
+    
+    def get_cart(self, cart):
+        pass
+    
+    def empty(self):
+        pass
     
